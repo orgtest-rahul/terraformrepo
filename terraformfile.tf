937442a5069d3962fbaadd0b904b6e507a9f20c2
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2021-10-14 23:46:02 +0530

    Create terraformfile.tf

---
+++ terraformfile.tf
@@ -0,0 +1,52 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Devops_Kochi"
+    storage_account_name = "rahul1storageaccount"
+    container_name       = "blobcontainer"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+  version = "~>2.0"
+  skip_provider_registration = true
+  features {}
+}
+variable "prefix"{
+  default = "rahul"
+}
+
+data "azurerm_resource_group" "rahulrg" {
+  name = "Devops_Kochi"
+}
+
+resource "azurerm_app_service_plan" "rahulappserviceplan" {
+  name                = "rahul-appserviceplan"
+  location            = azurerm_resource_group.rahulrg.location
+  resource_group_name = azurerm_resource_group.rahulrg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+resource "azurerm_app_service" "rahulappservice" {
+  name                = "rahul-app-service"
+  location            = azurerm_resource_group.rahulrg.location
+  resource_group_name = azurerm_resource_group.rahulrg.name
+  app_service_plan_id = azurerm_app_service_plan.rahulappserviceplan.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+
+}
+
+
