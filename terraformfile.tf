e76d0d9a687ccf1875abce480afd61212d8e3500
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2021-10-15 20:57:18 +0530

    Update terraformfile.tf

--- terraformfile.tf
+++ terraformfile.tf
@@ -14,10 +14,12 @@ terraform {
   }
 }
 
+# Resource group
 data "azurerm_resource_group" "rahulrg" {
   name = "Devops_Kochi"
 }
 
+# Appservice plan for web app 1
 resource "azurerm_app_service_plan" "rahulappserviceplan" {
   name                = "rahul-appserviceplan-1"
   location            = data.azurerm_resource_group.rahulrg.location
@@ -29,6 +31,8 @@ resource "azurerm_app_service_plan" "rahulappserviceplan" {
     size = "S1"
   }
 }
+
+# web App 1
 resource "azurerm_app_service" "rahulappservice" {
   name                = "rahultestwebapp04"
   location            = data.azurerm_resource_group.rahulrg.location
@@ -48,6 +52,7 @@ resource "azurerm_app_service" "rahulappservice" {
 
 }
 
+# ACR
 resource "azurerm_container_registry" "rahulacr" {
   name                = "rahultestwebapp04acr"
   resource_group_name = data.azurerm_resource_group.rahulrg.name
@@ -56,5 +61,50 @@ resource "azurerm_container_registry" "rahulacr" {
   admin_enabled       = true
 }
 
+# Service plan for web app 2
+resource "azurerm_app_service_plan" "service-plan" {
+  name = "rahul-appserviceplan-2"
+  location = data.azurerm_resource_group.rahulrg.location
+  resource_group_name = data.azurerm_resource_group.rahulrg.name
+  kind = "Linux"
+  reserved = true  
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }  
+}
+
+# web App 2
+resource "azurerm_app_service" "app-service" {
+  name = "rahultestwebapp08"
+  location = data.azurerm_resource_group.rahulrg.location
+  resource_group_name = data.azurerm_resource_group.rahulrg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+    
+    # Settings for private Container Registires  
+    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.rahulacr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.rahulacr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.rahulacr.admin_password
+  
+  }
+  # Configure Docker Image to load on start
+  site_config {
+    linux_fx_version = "DOCKER|${rahultestwebapp04acr}:${demo}"
+    always_on        = "true"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
+ 
+## Outputs
+output "app_service_name" {
+  value = "${azurerm_app_service.app-service.name}"
+}
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.app-service.default_site_hostname}"
+}
 
 
