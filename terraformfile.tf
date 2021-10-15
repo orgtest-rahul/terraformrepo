9b592db87539de66871e2597f1ca3badee041a3c
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2021-10-15 22:53:31 +0530

    Update terraformfile.tf

--- terraformfile.tf
+++ terraformfile.tf
@@ -101,6 +101,14 @@ resource "azurerm_app_service" "app-service" {
   }
 }
 
+## Outputs
+output "app_service_name" {
+  value = "${azurerm_app_service.app-service.name}"
+}
+output "app_service_default_hostname" {
+  value = "https://${azurerm_app_service.app-service.default_site_hostname}"
+}
+
 
 
 
