b0a712b55ca2c76839d4e77002dd9f55483753bd
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-09-19 07:51:11 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -28,8 +28,8 @@ resource "azurerm_resource_group" "rahulrgname" {
 
 resource "azurerm_storage_account" "storageccountname" {
   name                     = "storageaccountra"
-  resource_group_name      = "rah-rg1"
-  location                 = "North Europe"
+  resource_group_name      =  azurerm_resource_group.rahulrgname.name  
+  location                 = azurerm_resource_group.rahulrgname.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
