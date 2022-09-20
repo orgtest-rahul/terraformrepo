a90b5dee36253faff81ead2813b1fde40115af36
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-09-20 16:42:29 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -38,3 +38,17 @@ resource "azurerm_storage_account" "storageccountname" {
   }
 }
 
+resource "azurerm_storage_container" "storagecontainer" {
+  name                  = "radatacontainer"
+  storage_account_name  = azurerm_storage_account.storageccountname.name
+  container_access_type = "blob"
+}
+
+
+resource "azurerm_storage_blob" "maintf" {
+  name                   = "readme"
+  storage_account_name   = azurerm_storage_account.storageccountname.name
+  storage_container_name = azurerm_storage_container.storagecontainer.name
+  type                   = "Block"
+  source                 = "readme.md"
+}
