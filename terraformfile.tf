8ac0e2259ee35122fc4ec905a771dbeca05bb685
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-09-29 15:53:29 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -19,36 +19,57 @@ provider "azurerm" {
 }
 
 
+locals {
+  resource_group_name="rah-rg1"
+  resource_group_location="North Europe"
+  virtual_network={
+    name ="ra-virtual-network"
+    address_space = ["10.0.0.0/16"]
+    dns_servers = ["10.0.0.4", "10.0.0.5"]
+  }
+
+  subnets=[
+    {
+      name="subnet1"
+      address_prefix = "10.0.1.0/24"
+    },
+    {
+      name           = "subnet2"
+      address_prefix = "10.0.2.0/24"
+    }
+  ]
+
+}
 
 resource "azurerm_resource_group" "rahulrgname" {
-  name     = "rah-rg1"
-  location = "North Europe"
+  name     = local.resource_group_name
+  location = local.resource_group_location
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "example-security-group"
-  location            = azurerm_resource_group.rahulrgname.location
-  resource_group_name = azurerm_resource_group.rahulrgname.name
+  name                = "ra-network-security-group"
+  location            = local.resource_group_location
+  resource_group_name = local.resource_group_name
   depends_on = [
     azurerm_resource_group.rahulrgname
   ]
 }
 
 resource "azurerm_virtual_network" "VirtualNetwork" {
-  name                = "example-network"
+  name                = local.virtual_network.name
   location            = azurerm_resource_group.rahulrgname.location
   resource_group_name = azurerm_resource_group.rahulrgname.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = local.virtual_network.address_space
+  dns_servers         = local.virtual_network.dns_servers
 
   subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+    name           = local.subnets[0].name
+    address_prefix = local.subnets[0].address_prefix
   }
 
   subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+    name           = local.subnets[1].name
+    address_prefix = local.subnets[1].address_prefix
     security_group = azurerm_network_security_group.nsg.id
   }
 
@@ -59,3 +80,20 @@ resource "azurerm_virtual_network" "VirtualNetwork" {
     azurerm_network_security_group.nsg
   ]
 }
+
+# resource "azurerm_network_interface" "appnetworkinterface" {
+#   name                = "appnetworkinterface"
+#   location            = local.resource_group_location
+#   resource_group_name = local.resource_group_name
+
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.local.subnets[1].name.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+
+#     depends_on = [
+#     azurerm_virtual_network.VirtualNetwork
+#   ]
+
+# }
