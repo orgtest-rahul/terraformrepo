b0f1f554596b55cf5bda6cb6e7a7f3e857ffea39
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-09-29 16:34:02 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -55,9 +55,7 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "ra-network-security-group"
   location            = data.azurerm_resource_group.rahulrgname.location
   resource_group_name = data.azurerm_resource_group.rahulrgname.name
-  depends_on = [
-    azurerm_resource_group.rahulrgname
-  ]
+
 }
 
 # resource "azurerm_virtual_network" "VirtualNetwork" {
