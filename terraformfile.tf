11594a0628b4cf2c394d82155b9b7241a53c8a54
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-09-30 15:29:24 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -26,7 +26,7 @@ resource "azurerm_resource_group" "rahulrgname" {
 
 
 resource "azurerm_storage_account" "storageccountname" {
-  name                     = "rastorageaccount"
+  name                     = "rateststorageaccount"
   resource_group_name      = azurerm_resource_group.rahulrgname.name  
   location                 = azurerm_resource_group.rahulrgname.location
   account_tier             = "Standard"
