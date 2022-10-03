034be240fbdd4e64a9548788c37dfd48ea597c46
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-10-03 10:56:23 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -87,16 +87,16 @@ resource "azurerm_virtual_network" "VirtualNetwork" {
   ]
 }
 
-# resource "azurerm_subnet" "subnet2" {
-#   name                 = local.subnets[1].name
-#   resource_group_name  = azurerm_resource_group.rahulrgname.name
-#   virtual_network_name = azurerm_virtual_network.VirtualNetwork.name
-#   address_prefixes     = [local.subnets[1].address_prefix]
+resource "azurerm_subnet" "subnet2" {
+  name                 = local.subnets[1].name
+  resource_group_name  = azurerm_resource_group.rahulrgname.name
+  virtual_network_name = azurerm_virtual_network.VirtualNetwork.name
+  address_prefixes     = [local.subnets[1].address_prefix]
 
-#   depends_on = [
-#     azurerm_virtual_network.VirtualNetwork
-#   ]
-# }
+  depends_on = [
+    azurerm_virtual_network.VirtualNetwork
+  ]
+}
 
 # resource "azurerm_network_interface" "appnetworkinterface" {
 #   name                = "ra-appnetworkinterface"
