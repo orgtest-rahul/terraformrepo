c03a70f54a4347932481343b7701ef10405aa851
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-10-03 10:59:19 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -89,7 +89,7 @@ resource "azurerm_virtual_network" "VirtualNetwork" {
 
 resource "azurerm_subnet" "subnet2" {
   name                 = local.subnets[1].name
-  resource_group_name  = azurerm_resource_group.rahulrgname.name
+  resource_group_name  = data.azurerm_resource_group.rahulrgname.name
   virtual_network_name = azurerm_virtual_network.VirtualNetwork.name
   address_prefixes     = [local.subnets[1].address_prefix]
 
