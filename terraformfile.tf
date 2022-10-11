559cc3ac7172fa667cdaebf43bcdce97515c741b
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-10-11 15:45:34 +0530

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -21,18 +21,15 @@ data "azurerm_resource_group" "rahulrgname" {
 
 resource "azurerm_storage_account" "storageccountname" {
   name                     = "rahulstorageaccount"
-  resource_group_name      = azurerm_resource_group.rahulrgname.name  
-  location                 = azurerm_resource_group.rahulrgname.location
+  resource_group_name      = data.azurerm_resource_group.rahulrgname.name  
+  location                 = data.azurerm_resource_group.rahulrgname.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
     environment = "staging"
-  }
-  
-  depends_on = [
-    azurerm_resource_group.rahulrgname
-  ]
+  }  
+
 }
 
 resource "azurerm_storage_container" "storagecontainer" {
