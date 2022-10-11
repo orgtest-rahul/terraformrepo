0cd9dbd14aa331ad5626735e96b8ffaf0ad65b89
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-10-11 15:31:37 +0530

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     azurerm = {
@@ -11,106 +10,36 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = "da62005e-ffbb-483c-8af3-929e423ed9ce"
-  client_id       = "2722272e-a7c2-4475-bfe5-157ae43ac7c3"
-  client_secret   = "D_a8Q~9LYmKvCUFzRd4.JiUSmaJQGWfKIDIkNciK"
-  tenant_id       = "bd5c6713-7399-4b31-be79-78f2d078e543"
+ 
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rah-rg1"
-    storage_account_name = "rateststorageaccount"
-    container_name       = "ra-datacontainer"
-    key                  = "stage.terraform.tfstate"
-  }
+resource "azurerm_resource_group" "rahulrgname" {
+  name     = "Devops_kochi"
+  location = "West Europe"
 }
 
 
-locals {
-  resource_group_name="rah-rg1"
-  resource_group_location="North Europe"
-  virtual_network={
-    name ="ra-virtual-network"
-    address_space = ["10.0.0.0/16"]
-    dns_servers = ["10.0.0.4", "10.0.0.5"]
-  }
-
-  subnets=[
-    {
-      name="subnet1"
-      address_prefix = "10.0.1.0/24"
-    },
-    {
-      name           = "subnet2"
-      address_prefix = "10.0.2.0/24"
-    }
-  ]
-
-}
-
-# resource "azurerm_resource_group" "rahulrgname" {
-#   name     = local.resource_group_name
-#   location = local.resource_group_location
-# }
-
-
-data "azurerm_resource_group" "rahulrgname" {
-  name = local.resource_group_name
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "ra-network-security-group"
-  location            = data.azurerm_resource_group.rahulrgname.location
-  resource_group_name = data.azurerm_resource_group.rahulrgname.name
-
-}
-
-resource "azurerm_virtual_network" "VirtualNetwork" {
-  name                = local.virtual_network.name
-  location            = data.azurerm_resource_group.rahulrgname.location
-  resource_group_name = data.azurerm_resource_group.rahulrgname.name
-  address_space       = local.virtual_network.address_space
-  dns_servers         = local.virtual_network.dns_servers
-
-  subnet {
-    name           = local.subnets[0].name
-    address_prefix = local.subnets[0].address_prefix
-  }
+resource "azurerm_storage_account" "storageccountname" {
+  name                     = "rahul-storageaccount"
+  resource_group_name      = azurerm_resource_group.rahulrgname.name  
+  location                 = azurerm_resource_group.rahulrgname.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
   tags = {
     environment = "staging"
   }
+  
   depends_on = [
-    azurerm_network_security_group.nsg
+    azurerm_resource_group.rahulrgname
   ]
 }
 
-resource "azurerm_subnet" "subnet2" {
-  name                 = local.subnets[1].name
-  resource_group_name  = data.azurerm_resource_group.rahulrgname.name
-  virtual_network_name = azurerm_virtual_network.VirtualNetwork.name
-  address_prefixes     = [local.subnets[1].address_prefix]
-
+resource "azurerm_storage_container" "storagecontainer" {
+  name                  = "rahul-datacontainer"
+  storage_account_name  = azurerm_storage_account.storageccountname.name
+  container_access_type = "blob"
   depends_on = [
-    azurerm_virtual_network.VirtualNetwork
+    azurerm_storage_account.storageccountname
   ]
 }
-
-# resource "azurerm_network_interface" "appnetworkinterface" {
-#   name                = "ra-appnetworkinterface"
-#   location            = local.resource_group_location
-#   resource_group_name = local.resource_group_name
-
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnet2.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-
-#     depends_on = [
-#     azurerm_subnet.subnet2
-#   ]
-
-# }
