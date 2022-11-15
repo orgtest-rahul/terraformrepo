d3f29777b8b57a4abe4c70c18c2c6dd5ae819745
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2022-11-15 15:43:26 +0000

    test

--- terraformfile.tf
+++ terraformfile.tf
@@ -160,3 +160,31 @@ resource "azurerm_subnet_network_security_group_association" "rahulnsgassociatio
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+
+resource "azurerm_windows_virtual_machine" "rahulvm" {
+  name                = "rahulwindows-machine"
+  resource_group_name = local.resource_group_name
+  location            = local.resource_group_location
+  size                = "Standard_D2S_V3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.appnetworkinterface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [
+    azurerm_network_interface.appnetworkinterface
+  ]
+}
