ab38b85b0d0887413be7cb12aea878a5909581d2
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2023-01-20 19:58:54 +0530

    test

--- varibales.tf
+++ varibales.tf
@@ -1,7 +1,7 @@
 variable "number_of_subnets" {
     type = number 
     description = "This defines the number of subnets"
-    default = 3
+    default = 6
     validation {
         condition = var.number_of_subnets < 5
         error_message = "The number of subnet must be less than 5."
