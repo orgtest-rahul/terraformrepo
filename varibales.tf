f100019d0926d13bcd06c16b8e29e643549ff662
Rahul Reghunath <91956111+rahulreghu-mns@users.noreply.github.com>
2023-01-31 13:15:20 +0530

    Update varibales.tf

--- varibales.tf
+++ varibales.tf
@@ -1,11 +1,16 @@
 variable "number_of_subnets" {
     type = number 
     description = "This defines the number of subnets"
-    default = 6
+    default = 3
     validation {
         condition = var.number_of_subnets < 5
         error_message = "The number of subnet must be less than 5."
     
     }
-  
 }
+variable "number_of_Vms" {
+    type =number
+    description = "This defines the number of vms"
+    default =3
+}
+
